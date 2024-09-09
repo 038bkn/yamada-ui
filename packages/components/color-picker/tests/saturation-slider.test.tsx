b44ecc3ecb8556dbbde1f1198d6aae39cd6cf7ad
Yuto Urashima <yuto.urashima24@gmail.com>
2024-09-09 11:23:16 +0900

    test: add SaturationSlider component with pointer events

--- packages/components/color-picker/tests/saturation-slider.test.tsx
+++ packages/components/color-picker/tests/saturation-slider.test.tsx
@@ -1,4 +1,5 @@
 import { fireEvent, render, screen } from "@yamada-ui/test"
+import type { Hsv } from "../src"
 import { SaturationSlider } from "../src"
 
 describe("<SaturationSlider />", () => {
@@ -36,5 +37,32 @@ describe("<SaturationSlider />", () => {
 
     fireEvent.keyDown(slider, { key: "End" })
     expect(mockOnChange).toHaveBeenCalledWith([0, 0.01, 0.99])
+  })
+
+  test("SaturationSlider component with pointer events", async () => {
+    const onChange = vi.fn()
+    const onChangeStart = vi.fn()
+    const onChangeEnd = vi.fn()
+
+    const defaultValue: Hsv = [180, 1, 1]
+
+    const { user } = render(
+      <SaturationSlider
+        defaultValue={defaultValue}
+        onChange={onChange}
+        onChangeStart={onChangeStart}
+        onChangeEnd={onChangeEnd}
+      />,
+    )
+
+    const slider = screen.getByRole("slider")
+
+    await user.tab()
+    await user.pointer({ keys: "[MouseLeft>]", target: slider })
+    await user.pointer({ keys: "[/MouseLeft]", target: slider })
+
+    expect(onChangeStart).toHaveBeenCalledWith([180, 1, 1])
+    expect(onChange).toHaveBeenCalledWith([180, 0, 0])
+    expect(onChangeEnd).toHaveBeenCalledWith([180, 0, 0])
   })
 })
